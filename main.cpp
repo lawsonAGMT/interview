f4565cc9f7c415215c977802c1585ef405b7714c
Lawson Fulton <lawson@augmenta.ai>
2021-02-11 12:56:00 -0500

    switching to to_string

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 int
 main()
 {
-    const intvw::Vector2d p1(1.0, 2.0);
-    const intvw::Vector2d p2(3.0, 4.0);
+    const intvw::Vector2 p1(1.0, 2.0);
+    const intvw::Vector2 p2(3.0, 4.0);
 
     auto p3 = p1 + p2;
 
     std::cout << "Expected output: 4 6" << std::endl;
-    std::cout << p3 << std::endl;
+    std::cout << p3.to_string() << std::endl;
 }
